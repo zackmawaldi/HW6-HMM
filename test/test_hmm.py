7ed6fd906e6b03f5224a66294cd002b6c9d7f5b3
zack <yazan.mawaldi@gmail.com>
2024-02-22 11:41:18 -0800

    implement testing

--- test/test_hmm.py
+++ test/test_hmm.py
@@ -7,7 +7,6 @@ import numpy as np
 
 def test_mini_weather():
     """
-    TODO: 
     Create an instance of your HMM class using the "small_weather_hmm.npz" file. 
     Run the Forward and Viterbi algorithms on the observation sequence in the "small_weather_input_output.npz" file.
 
@@ -22,21 +21,36 @@ def test_mini_weather():
     mini_hmm=np.load('./data/mini_weather_hmm.npz')
     mini_input=np.load('./data/mini_weather_sequences.npz')
 
+    hmm = HiddenMarkovModel(**mini_hmm)
 
 
+    # bad seq check
+    bad_sequence = ['sunny', '--APPLE--', 'rainy']
+    with pytest.raises(ValueError):
+        hmm.forward( np.array( bad_sequence ) )
+        hmm.viterbi( np.array( bad_sequence ) )
 
 
+    # empty input check
+    empty = np.empty(0)
+    assert hmm.forward(empty) == 0
+    assert hmm.viterbi(empty) == []
 
-    
-   
-    pass
+
+    # forward test
+    test_forward = hmm.forward(mini_input['observation_state_sequence'])
+    assert round(test_forward, 3) == 0.035
+
+
+    # viterbi test
+    test_bestpath = hmm.viterbi(mini_input['observation_state_sequence'])
+    assert test_bestpath == list( mini_input['best_hidden_state_sequence'] )
 
 
 
 def test_full_weather():
 
     """
-    TODO: 
     Create an instance of your HMM class using the "full_weather_hmm.npz" file. 
     Run the Forward and Viterbi algorithms on the observation sequence in the "full_weather_input_output.npz" file
         
@@ -44,18 +58,12 @@ def test_full_weather():
     Assert that the state sequence returned is in the right order, has the right number of states, etc. 
 
     """
+    full_hmm=np.load('./data/full_weather_hmm.npz')
+    full_input=np.load('./data/full_weather_sequences.npz')
 
-    pass
+    hmm = HiddenMarkovModel(**full_hmm)
 
-
-
-
-
-
-
-
-
-
-
-
+    # viterbi test
+    test_bestpath = hmm.viterbi(full_input['observation_state_sequence'])
+    assert test_bestpath == list( full_input['best_hidden_state_sequence'] )
 

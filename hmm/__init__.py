fde7ea3c0a85a8cc3d468300b7038e261431d735
zack <yazan.mawaldi@gmail.com>
2024-02-22 11:41:47 -0800

    make files to allow pip-installable

--- hmm/__init__.py
+++ hmm/__init__.py
@@ -1,9 +1,7 @@
 """
 UCSF BMI203: Biocomputing Algorithms 2024
-Author:
-Date:
-Package: 
-Description: 
+Author: Zack Mawaldi
+Date: Feb 22 2024
 """
 import numpy as np
 from .hmm import HiddenMarkovModel
